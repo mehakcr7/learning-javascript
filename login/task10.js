42363de151e2bfb8129d58e72873f1f1f2da7bdf
Mehak Vazhayil <mehakvazhayil2002@gmail.com>
2024-08-22 13:54:37 +0530

    Add multiple users and set password requirement to greater than 8 characters

--- login/task10.js
+++ login/task10.js
@@ -1,58 +1,57 @@
-var username = document.getElementById('username')
-function fn_username() {
-    var name_value = username.value
-    console.log(name_value);
-
-
-
-
-}
-
-
-var password = document.getElementById('password')
-function fn_pass() {
-    var pass_value = password.value
-    var name_value = username.value
-    console.log(pass_value);
-    console.log(name_value);
-
-
-
-
-
-}
-
 function submitt() {
-    var pass_value = password.value
-    var name_value = username.value
+  // to retrieve data from local storage
+  var flag = 0;
+
+  var stored_details = localStorage.getItem("personinfo");
+  if (stored_details) {
+    var person = JSON.parse(stored_details);
+  } else {
+    alert("Account not found.");
+  }
+
+  console.log(stored_details);
+  console.log(person);
+
+  // user input
+
+  var get_userpass = password.value;
+
+  var get_username = username.value;
+
+  console.log("get name" + get_username);
+  console.log("get pass " + get_userpass);
+
+  // loop for matching
+
+  for (let i = 0; i < person.length; i++) {
+    var pass_value = person[i].password;
+    var name_value = person[i].username;
     console.log(pass_value);
-    var get_userpass = localStorage.getItem('password');
-    var get_username = localStorage.getItem('username');
-    console.log("get name" + get_username);
-    console.log("get pass " + get_userpass);
+    console.log(name_value);
 
+    if (name_value === get_username && pass_value === get_userpass) {
+      console.log("yes");
+      var element = document.getElementById("display");
+      var newelement = document.getElementById("box1id");
+      element.style.display = "none";
+      var newdiv = document.createElement("div");
+      newelement.append(newdiv);
+      newdiv.style.display = "flex";
+      newdiv.style.justifyContent = "center";
+      newdiv.style.alignItems = "center";
+      newdiv.style.height = "100%";
+      newdiv.innerHTML = "LOGIN SUCCESSFUL";
 
-    if (name_value == get_username && pass_value == get_userpass) {
-        console.log("yes");
-        var element = document.getElementById('display')
-        var newelement = document.getElementById('box1id')
-        element.style.display = "none";
-        var newdiv = document.createElement('div')
-        newelement.append(newdiv)
-        newdiv.style.display = "flex"
-        newdiv.style.justifyContent = "center"
-        newdiv.style.alignItems = "center"
-        newdiv.style.height = "100%"
-        newdiv.innerHTML = "LOGIN SUCCESSFUL"
-
-
-
+      flag = 1;
+      break;
+    } else {
+      console.log("no");
     }
-    else {
-        alert("Invalid username/password!")
-    }
+  }
 
+  // conditions
 
+  if (flag == 0) {
+    alert("Invalid username or password!");
+  }
 }
-
-
--- login/task10.signup.js
+++ login/task10.signup.js
@@ -1,74 +1,107 @@
-var username = document.getElementById('username')
+var username = document.getElementById("username");
+var pass_value = document.getElementById("password");
+var con_pass_value = document.getElementById("confirm_password");
 
-let details = {}
-function fn_username() {
-    var name_value = username.value
-   
-    localStorage.setItem('username', name_value);
-    details.username = name_value
-
-
-}
-
-
-var pass_value = document.getElementById('password')
-function fn_pass() {
-    var pass_value = password.value
-    localStorage.setItem('password', pass_value);
-    details.password = pass_value
-
-
-
-}
-var con_pass_value = document.getElementById('confirm_password')
-function fn_confirm_pass() {
-    var con_pass_value = confirm_password.value
-    localStorage.setItem('con_password', con_pass_value);
-    details.confirm_password = con_pass_value
-
-}
-
+var details = [];
+var person = {};
+var flag = 0;
 
 function login_btn() {
+  // username
+  var name_value = username.value;
+  person.username = name_value;
 
-    addEventListener("click", () => {
-        
+  //confirm password
+  var con_pass_value = confirm_password.value;
 
-        let pass_value = localStorage.getItem('password')
-        let con_pass_value = localStorage.getItem('con_password')
+  // password
+  var pass_value = password.value;
+  person.password = pass_value;
 
-        if (pass_value == con_pass_value) {
-            console.log(pass_value);
-            console.log(con_pass_value);
+  // pushing to array
 
+  var storedDetails = localStorage.getItem("personinfo");
+  if (storedDetails) {
+    details = JSON.parse(storedDetails);
+  }
 
+  // console.log("pushed array " + person);
+  console.log("pushed array length" + details.length);
 
-        var element = document.getElementById('display')
-        var newelement = document.getElementById('box1id')
+  for (let i = 0; i < details.length; i++) {
+    console.log(details[i].username);
 
-        element.style.display = "none";
-        var newdiv = document.createElement('div')
-        var atag = document.createElement('a')
-        atag.textContent = "click me"
-        newelement.append(newdiv)
-        newelement.append(atag)
-        atag.setAttribute("href", "task10.html")
-        atag.innerText = "LOGIN PAGE"
-        atag.style.display = "flex"
-        atag.style.justifyContent = "center"
-        atag.style.alignItems = "center"
-        newdiv.style.display = "flex"
-        newdiv.style.justifyContent = "center"
-        newdiv.style.alignItems = "center"
-        newdiv.style.height = "50%"
-        newdiv.innerHTML = "PROFILE CREATED"
+    
+
+    // repeated username || existing username
+
+    if (details[i].username === name_value) {
+      console.log(details[i].username);
+      console.log(name_value);
+      alert("The username already exists.");
+
+      flag = 1;
+      break;
     }
-        else {
-            alert('Password does not match')
-        }
+
+    
 
 
 
+    // check 8 letter
 
-    })
+    if (pass_value.length < 8) {
+      alert("Password must be at least 8 characters long.");
+      flag = 1;
+      break;
+    }
+
+    
+
+
+    // flag===0 , true condition
+  }
+  if (flag === 0) {
+    if (pass_value === con_pass_value && pass_value !== "") {
+      details.push(person);
+
+      var element = document.getElementById("display");
+      var newelement = document.getElementById("box1id");
+
+      element.style.display = "none";
+      var newdiv = document.createElement("div");
+      var atag = document.createElement("a");
+      var atag2 = document.createElement("a");
+
+      newelement.append(newdiv);
+      newelement.append(atag);
+      newelement.append(atag2);
+      atag.setAttribute("href", "task10.html");
+      atag2.setAttribute("href", "task10signup.html");
+      atag.innerText = "LOGIN PAGE";
+      atag2.innerText = "SIGNUP PAGE";
+      atag.style.display = "flex";
+      atag.style.justifyContent = "center";
+      atag.style.alignItems = "center";
+      atag2.style.display = "flex";
+      atag2.style.justifyContent = "center";
+      atag2.style.alignItems = "center";
+      newdiv.style.display = "flex";
+      newdiv.style.justifyContent = "center";
+      newdiv.style.alignItems = "center";
+      newdiv.style.height = "50%";
+      newdiv.textContent = "PROFILE CREATED";
+
+      const userJSON = JSON.stringify(details);
+      localStorage.setItem("personinfo", userJSON);
+      console.log(userJSON);
+      // console.log("pass val = " + pass_value);
+    } else if (pass_value != con_pass_value) {
+      alert("The passwords do not match.");
+    } else if (name_value === "" || pass_value === "") {
+      alert("The form is incomplete.");
+    }
+  }
+
+  
 }
